2f8b06a4385dc8cadf1fe985882e4bb0c681ae74
Mario Moreno <m4mc3r@gmail.com>
2025-01-25 10:22:08 -0300

    Remove hardcoded configs, replace with env variables

--- main.go
+++ main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"database/sql"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 
 	"github.com/gin-gonic/gin"
 )
 
-func getDB() *sql.DB {
+func getDB(dbdriver string, dbsource string) *sql.DB {
 	var err error
-	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/cookbook")
+	db, err := sql.Open(dbdriver, dbsource)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -19,12 +20,16 @@ func getDB() *sql.DB {
 }
 
 func main() {
+	port := os.Getenv("FLAT_PORT")
+	dbdriver := os.Getenv("FLAT_DB_DRIVER_NAME")
+	dbsource := os.Getenv("FLAT_DB_DATA_SOURCE_NAME")
+
 	g := gin.Default()
 
-	r := NewRepository(getDB())
+	r := NewRepository(getDB(dbdriver, dbsource))
 	s := NewService(r)
 
 	Handle(g, s)
 
-	g.Run(":2000")
+	g.Run(":" + port)
 }
